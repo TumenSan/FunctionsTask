763779df22273d791668fe4f09464a51a19e95e2
Tumen02 <sandanov.tumen.sib@gmail.com>
2024-02-29 02:02:52 +0700

    feat: add model

---
+++ FunctionsTask/Model/FunctionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsTask.Model
+{
+    public class FunctionModel
+    {
+        public string Name { get; set; }
+        public double CoefficientA { get; set; }
+        public double CoefficientB { get; set; }
+        public double CoefficientC { get; set; }
+    }
+}
