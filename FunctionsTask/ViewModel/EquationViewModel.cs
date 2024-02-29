60b28bcc0c97dac6830e6cb352d460dc54325170
Tumen02 <sandanov.tumen.sib@gmail.com>
2024-02-29 19:06:27 +0700

    feat: add design elements

--- FunctionsTask/ViewModel/EquationViewModel.cs
+++ FunctionsTask/ViewModel/EquationViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FunctionsTask.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -7,17 +9,41 @@ using System.Threading.Tasks;
 
 namespace FunctionsTask.ViewModel
 {
-    public class EquationViewModel
+    public class FunctionViewModel
     {
+        public ObservableCollection<FunctionModel> Equations { get; set; } = new ObservableCollection<FunctionModel>();
+
+        private double _CoefficientA;
+        public double CoefficientA;
+
+        private double _CoefficientB;
+        public double CoefficientB;
+
+        private double _CoefficientC;
+        public double CoefficientC;
+
         private void RecognitionDegrees()
         {
             // EquationDegreeX = Type * ...
             // EquationDegreeY = Type * ...
         }
+
+        private void AddCoefficients()
+        {
+            Equations.Add(new FunctionModel { CoefficientA = CoefficientA, CoefficientB = CoefficientB, CoefficientC = CoefficientC });
+        }
+
         private void Calculate()
         {
-            // Реализация вычисления результата для каждого уравнения
-            // f(x,y) = (CoefficientA * X ^ EquationDegreeX) + (CoefficientB * Y ^ EquationDegreeY) + CoefficientC
+            // Реализация вычисления результата для каждого уравнения    
+            // Result = (CoefficientA * X ^ EquationDegreeX) + (CoefficientB * Y ^ EquationDegreeY) + CoefficientC
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
