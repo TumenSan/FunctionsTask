573eacb6b1f1601dfd921a41ef526fd38e42ca10
Tumen02 <sandanov.tumen.sib@gmail.com>
2024-03-01 14:54:59 +0700

    feat: update model in private

--- FunctionsTask/Model/FunctionModel.cs
+++ FunctionsTask/Model/FunctionModel.cs
@@ -8,12 +8,54 @@ namespace FunctionsTask.Model
 {
     public class FunctionModel
     {
-        public string Type { get; set; }
-        public double CoefficientA { get; set; }
-        public double CoefficientB { get; set; }
-        public double CoefficientC { get; set; }
-        public double X { get; set; }
-        public double Y { get; set; }
-        public double Result { get; set; }
+        private string _type;
+        private double _coefficientA;
+        private double _coefficientB;
+        private double _coefficientC;
+        private double _x;
+        private double _y;
+        private double _result;
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public double CoefficientA
+        {
+            get { return _coefficientA; }
+            set { _coefficientA = value; }
+        }
+
+        public double CoefficientB
+        {
+            get { return _coefficientB; }
+            set { _coefficientB = value; }
+        }
+
+        public double CoefficientC
+        {
+            get { return _coefficientC; }
+            set { _coefficientC = value; }
+        }
+
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public double Result
+        {
+            get { return _result; }
+            set { _result = value; }
+        }
     }
 }
