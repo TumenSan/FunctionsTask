e3cfd75b071599ff272f830b97726837b1cb9531
Tumen02 <sandanov.tumen.sib@gmail.com>
2024-03-01 20:29:01 +0700

    feat: add Test Project

---
+++ FunctionsTest/FunctionsTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FunctionsTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
