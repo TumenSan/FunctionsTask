10a9ea53c7486040058fa6d62ad1956b8906726f
Tumen02 <sandanov.tumen.sib@gmail.com>
2024-03-04 02:50:14 +0700

    feat: add quadr and cubic tests

--- FunctionTest/FunctionTest.cs
+++ FunctionTest/FunctionTest.cs
@@ -32,5 +32,43 @@ namespace FunctionTest
 
             Assert.That(result, Is.EqualTo(6));
         }
+
+
+        [Test]
+        public void CalculateResult_QuadraticFunction()
+        {
+            var function = new FunctionModel
+            {
+                Type = "Квадратичная",
+                CoefficientA = 1,
+                CoefficientB = 2,
+                CoefficientC = 3,
+                X = 2,
+                Y = 2
+            };
+
+            var result = _viewModel.CalculateResult(function);
+
+            Assert.That(result, Is.EqualTo(15));
+        }
+
+
+        [Test]
+        public void CalculateResult_CubicFunction()
+        {
+            var function = new FunctionModel
+            {
+                Type = "Кубическая",
+                CoefficientA = 1,
+                CoefficientB = 2,
+                CoefficientC = 3,
+                X = 3,
+                Y = 3
+            };
+
+            var result = _viewModel.CalculateResult(function);
+
+            Assert.That(result, Is.EqualTo(42));
+        }
     }
 }
